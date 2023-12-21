b55891decd37293c3cc2ff14170fe0102dcd8fe9
Tamirlan <ryankahito@gmail.com>
2023-12-21 20:45:28 +0300

    feat: add Signup component

--- src/components/Auth/signup/Signup.tsx
+++ src/components/Auth/signup/Signup.tsx
@@ -1,3 +1,73 @@
+import { SyntheticEvent, useRef, useState } from "react";
+import styles from "./signup.module.css";
+import { IAuthUser } from "../../../utils/types";
+
 export const Signup = () => {
-  return <div>Signup</div>;
+  const [errMessage, setErrMessage] = useState("");
+  const users: IAuthUser[] = JSON.parse(localStorage.getItem("users") ?? "[]");
+
+  const username = useRef<HTMLInputElement>(null);
+  const password = useRef<HTMLInputElement>(null);
+  const repeatPassword = useRef<HTMLInputElement>(null);
+
+  const handleSignup = (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    if (users.find((user) => user.username !== username.current?.value)) {
+      setErrMessage(`user named ${username.current?.value} exists`);
+      return;
+    }
+
+    if (password.current?.value !== repeatPassword.current?.value) {
+      setErrMessage("passwords do not match");
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSignup}
+      action=""
+      className={styles.form}
+    >
+      <h1>Welcome!</h1>
+      <div>
+        <label htmlFor="username">username</label>
+        <input
+          required
+          type="text"
+          id="username"
+          ref={username}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">password</label>
+        <input
+          required
+          type="password"
+          id="password"
+          ref={password}
+        />
+      </div>
+      <div>
+        <label htmlFor="repeat-password">repeat password</label>
+        <input
+          required
+          type="text"
+          id="repeat-password"
+          ref={repeatPassword}
+        />
+      </div>
+      {errMessage && <p className={styles.error}>{errMessage}</p>}
+      <button>Sign up</button>
+      <footer>
+        <label htmlFor="to-signin">Already have an account?</label>
+        <button
+          type="button"
+          id="to-signin"
+        >
+          <a href="/signin">Sign in!</a>
+        </button>
+      </footer>
+    </form>
+  );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,8 +4,10 @@ import { Search } from "./Search/Search";
 import styles from "./header.module.css";
 
 export const Header = () => {
+  const isAuthPage = location.pathname.includes("sign");
+
   return (
-    <header className={styles.header}>
+    <header className={isAuthPage ? styles.hide : styles.header}>
       <Nav />
       <Search />
       <AuthButtons />
